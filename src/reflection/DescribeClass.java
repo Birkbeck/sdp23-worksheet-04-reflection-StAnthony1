ab60de5856b2cd18d4c7ef9f92f68d1404db4f35
Oz O'Neill <oswald.oneill@gmail.com>
2023-02-13 09:45:44 +0000

    Describe Class complete and pretty printing

--- src/reflection/DescribeClass.java
+++ src/reflection/DescribeClass.java
@@ -1,10 +1,28 @@
 package reflection;
+import javax.sound.midi.Soundbank;
+import javax.swing.*;
+import java.util.Arrays;
+import java.lang.Integer;
+
 
 public class DescribeClass {
-    public static void main(String[] args) {
+
+    public static void main(String[] args) throws ClassNotFoundException, NoSuchMethodException {
         if (args.length != 1)
             System.out.println("Usage: "); // TODO: describe how to use the utility
 
-        // TODO: implement the functionality for Question 1
+        else {
+            Class<?> obj = Class.forName(args[0]);
+            System.out.println(obj.getCanonicalName());
+            System.out.printf("1. Constrictors of %s:%n", obj);
+            System.out.println(Arrays.toString(obj.getDeclaredConstructors()));
+            System.out.printf("2. Modifiers of %s:%n", obj);
+            System.out.println(obj.getModifiers());
+            System.out.printf("3. Fields of %s:%n", obj);
+            System.out.println(Arrays.toString((obj.getDeclaredFields())));
+            System.out.printf("4. Methods of %s:%n", obj);
+            System.out.println(Arrays.toString(obj.getMethods()));
+
+            }
     }
 }
