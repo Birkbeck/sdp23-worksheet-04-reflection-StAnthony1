c385e23bbd7e8596ad1811d326b0ca30bb19f4c0
Oz O'Neill <oswald.oneill@gmail.com>
2023-02-13 15:00:01 +0000

    Begun AutoGrader helper method test

--- test/reflection/Autograder.java
+++ test/reflection/Autograder.java
@@ -1,20 +1,46 @@
 package reflection;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+
 
 public class Autograder {
 
-    static final String CLASS_NAME = ""; // TODO: insert the class name to be tested
+    static final String CLASS_NAME = "java.lang.Integer"; // TODO: insert the class name to be tested
 
     @Test
-    public void testMoreThanFourFields() {
-        // TODO: add testing code
+    public void testMoreThanFourFields() throws ClassNotFoundException {
+        Class<?> testClass = Class.forName(CLASS_NAME);
+        assert (5 > testClass.getDeclaredFields().length);
     }
 
     @Test
-    public void testNonPrivateFields() {
-        // TODO: add testing code
+    public void testNonPrivateFields() throws ClassNotFoundException {
+        Class<?> testClass = Class.forName(CLASS_NAME);
+        Field[] fields = testClass.getDeclaredFields();
+        for (Field f : fields) {
+            Assertions.assertNotEquals(2, f.getModifiers());
+        }
     }
 
-    // TODO: add a test for each of the remaining items of Question 3
+    @Test
+    public void testNoArrayListField() throws ClassNotFoundException {
+        Class<?> testClass = Class.forName(CLASS_NAME);
+        Field[] fields = testClass.getDeclaredFields();
+        for (Field f : fields) {
+            Assertions.assertNotEquals(f.getGenericType(), ArrayList.class);
+        }
+    }
+
+    @Test
+    public void fewerthan2privatehelpers() throws ClassNotFoundException{
+        Class<?> testclass = Class.forName(CLASS_NAME);
+        // TODO: find out how to distinguish a helper method from another private method
+
+    }
 }
+
